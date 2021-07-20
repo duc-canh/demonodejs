5613f23710239ce2bace4ad5d6109056fc37cbc3
duc-canh <84029160+duc-canh@users.noreply.github.com>
2021-07-20 19:49:58 +0700

    demonodejs

--- index.js
+++ index.js
@@ -1,81 +1,59 @@
 var express = require("express");
-var app = express(); // tao 1 ung dung tu express module
-var port = process.env.PORT || 5000;
+var app = express();// tao 1 ung dung tu express module
+var port = 5000;// cong vao
 app.listen(port,function (){
-    console.log("Server is running....");
+    console.log("app is running...");
 });
-// cap quyen truy cap cac abn file static (css, img, jquery...)
+// tren bat dau phai viet
+// cap quyen truy cap cac file tinh
 app.use(express.static("public"));
-app.set("view engine","ejs");// Báo rằng sẽ sử dụng ejs làm view engine
-var count = 0;// dem so luong nguoi vao web
-var products = [
+
+app.set("view engine","ejs");//báo rằng dùng ejs để làm view engine
+var count = 0;
+var product =[
     {
         id:1,
-        name:"Product 1",
-        image:"images/dtvn.jpg"
-    },
-    {
-        id:2,
-        name:"Product 2",
-        image:"images/dtvn.jpg"
+        name:"product 1",
+        img:"../imagegroup/shirts.jpg"
     },
     {
         id:3,
-        name:"Product 3",
-        image:"images/dtvn.jpg"
+        name:"product 3",
+        img:"../imagegroup/shirts.jpg"
     },
     {
-        id:4,
-        name:"Product 4",
-        image:"images/dtvn.jpg"
+        id:2,
+        name:"product 2",
+        img:"../imagegroup/shirts.jpg"
     }
-];
-// routing - bo dinh tuyen (nhan vien cua van phong)
-app.get("/",function (req,res){ // trang chu
-    // res.send("<html><head><title</head></html>"); // html- chỉ là 1 chuỗi đối với máy chủ
-    // res.sendFile(__dirname+"/public/demobootstrap.html");  //__dirname -> xuất cho chúng ta 1 chuỗi đường dẫn chính xác trên máy tính đến thư mục này
-    count++;
+]
+// rounting bo dinh tuyen
+app.get("/",function (req,res){
+    //res.sendFile(__dirname+"/public/assignment/as3.html");
+   count++;
+   ;
     res.render("home",{
-        count: count
-    });// tu dong hieu de lay file home.ejs trong thu muc views
+        product:product
+    });//tự động hiểu lấy file home,ejs trong thư mực
 });
-
 app.get("/ke-toan",function (req,res){
-    // res.send("Xin chao, vui long nop tien..");
-    // res.sendFile(__dirname+"/public/assignment3.html");
-    count++;
-    res.render("ketoan",{
-        products:products
-    });
+    res.sendFile(__dirname+"/public/assignment/asjs5/asjs5.html");
+});
+app.get("/chitier",function (req,res){
+   res.render("chitiet");
 });
 
-app.get("/chi-tiet",function (req,res){
-    var masp = req.query.id;
-    // khi co id, tim kiem theo id trong array
-    var p = {};
-    for(var i=0;i<products.length;i++){
-        if(products[i].id == masp){
-            p = products[i];
-            break;
-        }
-    }
-    res.render("chitiet",{
-        masp:masp,
-        p: p
-    });
-});
-// su dung tham so tinh
-app.get("/chi-tiet2/:id",function (req,res){
+app.get("/chitiet2/:id",function (req,res){
     var masp = req.params.id;
-    var p = {};
-    for(var i=0;i<products.length;i++){
-        if(products[i].id == masp){
-            p = products[i];
+    var p ={};
+    for(var i=0;i<product.length;i++){
+        if(product[i] == masp){
+            p= product[i];
             break;
         }
     }
     res.render("chitiet",{
-        masp:masp,
-        p: p
+        nasp:masp,
+        p:p
     });
 });
